6f19b8e68ebb9738944b48e35e8151cd06749f86
Pranay Periwal <pranayperiwal24@gmail.com>
2022-12-27 19:48:16 +0530

    file structure changed

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ import whisper
 import os
 import openai
 
-app = Flask(__name__ , static_folder='../build', static_url_path='/')
+app = Flask(__name__ , static_folder='client/build', static_url_path='/')
 CORS(app)
 
 
